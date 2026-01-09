ba1351ddefc93d2db656be95f8e32fd072b818ab
DeltaGr <celichter@gmail.com>
2026-01-09 14:08:02 +0100

    Add Listings controller, index and new views, functional form

---
+++ app/controllers/listings_controller.rb
@@ -0,0 +1,28 @@
+class ListingsController < ApplicationController
+  def index
+    @listings = Listing.all
+  end
+
+  def show
+    @listing = Listing.find(params[:id])
+  end
+
+  def new
+    @listing = Listing.new
+  end
+
+  def create
+    @listing = current_user.listings.build(listing_params)
+    if @listing.save
+      redirect_to @listing, notice: "Listing successfully created!"
+    else
+      render :new, status: :unprocessable_entity
+    end
+  end
+
+  private
+
+  def listing_params
+    params.require(:listing).permit(:title, :description, :price)
+  end
+end
--- config/routes.rb
+++ config/routes.rb
@@ -1,11 +1,19 @@
 Rails.application.routes.draw do
+  get 'listings/index'
+  get 'listings/show'
+  get 'listings/new'
+  get 'listings/create'
   devise_for :users
   # Define your application routes per the DSL in https://guides.rubyonrails.org/routing.html
 
+  # Listings-Routen: index, show, new, create
+  resources :listings, only: [:index, :show, :new, :create]
   # Reveal health status on /up that returns 200 if the app boots with no exceptions, otherwise 500.
   # Can be used by load balancers and uptime monitors to verify that the app is live.
   get "up" => "rails/health#show", as: :rails_health_check
 
   # Defines the root path route ("/")
   # root "posts#index"
+  # Root: Startseite die alle Listings zeigen soll
+  root to: "listings#index"
 end
